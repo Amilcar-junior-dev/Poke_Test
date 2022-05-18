18d23b4cdf00012bddd75204d644b63c9faed655
Amilcar_Junior_dev <amilcarjuniordev@gmail.com>
2022-05-18 01:26:50 -0300

    create page home

---
+++ src/Atomic/Atoms/Box/Models/index.d.ts
@@ -0,0 +1,49 @@
+
+export interface ContainerProps {
+    flex?: number
+    width?: string | number
+    heigth?: string | number
+    padding?: string | number
+    alignItems?: 'center' | 'stretch' | 'flex-start' | 'flex-end' | 'baseline'
+    justifyContent?: 'baseline' | 'center' | 'flex-end'|'flex-start'
+    flexDirections?: 'row' | 'column' | 'row-reverse' |'column-reverse'
+    backgroundColor?: string
+}
+
+export interface BoxHeaderProps {
+    width?: string | number
+    heigth?: string | number
+    padding?: string | number
+    alignItems?: 'center' | 'stretch' | 'flex-start' | 'flex-end' | 'baseline'
+    justifyContent?: 'baseline' | 'center' | 'flex-end'|'flex-start' | 'space-around' | 'space-between'
+    flexDirections?: 'row' | 'column' | 'row-reverse' |'column-reverse'
+    backgroundColor?: string
+    marginBottom?: string | number 
+    marginTop?: string | number 
+    marginLeft?: string | number
+    marginRight?: string | number
+    
+}
+
+export interface BoxProps {
+    flex?: number
+    width?: string | number
+    height?: string | number 
+    pd?: number 
+    alignItems?: 'center' | 'stretch' | 'flex-start' | 'flex-start' | 'flex-end' | 'baseline'
+    justifyContent?: 'baseline' | 'center' | 'flex-end'|'flex-start' | 'space-around' | 'space-between'
+    backgroundColor?: string;
+    marginBottom?: string | number 
+    marginTop?: string | number 
+    marginLeft?: string | number
+    marginRight?: string | number 
+    borderRadius?: string | number
+    borderRadiusTopLeft?: string | number 
+    borderRadiusTopRight?: string | number
+    borderRadiusBottomLeft?: string | number
+    borderRadiusBottomRight?: string | number
+    borderColor?: string 
+    borderWidth?: string
+    position?: 'absolute' | 'fixed' | 'relative' | 'static' | 'sticky'
+    flexDirections?: 'row' | 'column' | 'row-reverse' |'column-reverse'
+}
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import View from './view'
+
+const Home = () => {
+    return (
+        <View />
+    )
+}
+export default Home;
